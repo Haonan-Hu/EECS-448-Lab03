531f209c72487216ea265ffa82b76c5ffa45367a
Haonan Hu <hhn97@ku.edu>
2019-09-29 11:03:34 -0500

    fixed ex2 problems

--- Exercise2/ex2.js
+++ Exercise2/ex2.js
@@ -2,20 +2,26 @@ var collection  = ['cat1.jpeg', 'cat2.jpeg', 'cat3.jpeg', 'cat4.jpeg', 'cat5.jpe
 var curImg = 0
 function previous()
 {
-  curImg--;
-  if(curImg < 0)
+  if(curImg == 0)
   {
-    curImg = 5;
+    curImg = 4;
+  }
+  else
+  {
+    curImg--;
   }
   document.images[0].src = collection[curImg];
 }
 
 function next()
 {
-  curImg++;
-  if(curImg > 5)
+  if(curImg == 4)
   {
     curImg = 0;
+  }
+  else
+  {
+    curImg++;
   }
   document.images[0].src = collection[curImg];
 }
